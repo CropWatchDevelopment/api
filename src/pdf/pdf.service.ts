85b89ce8616c720e8ca9e16fea9e973b7577e5b4
Kevin Cantrell <kevin@cropwatch.io>
2024-11-03 02:09:33 +0900

    showing off serialized document deffinition

--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -5,7 +5,7 @@ import { TDocumentDefinitions, TFontDictionary } from 'pdfmake/build/pdfmake';
 
 @Injectable()
 export class PdfService {
-    
+
     public async createPdfBinary(): Promise<Buffer> {
         console.log('path', join(process.cwd(), 'dist', '', 'src/pdf/fonts/Roboto/Roboto-Regular.ttf'));
         const fonts: TFontDictionary = {
@@ -16,13 +16,62 @@ export class PdfService {
                 bolditalics: join(process.cwd(), 'dist', '', 'src/pdf/fonts/Roboto/Roboto-MediumItalic.ttf'),
             },
         };
-        const printer: PdfPrinter = new PdfPrinter(fonts);
-        const docDefinition: any = {
+
+        const report = JSON.stringify({
+            permissions: {
+                printing: 'highResolution',
+                modifying: false,
+                copying: true,
+                annotating: true,
+                fillingForms: true,
+                contentAccessibility: true,
+                documentAssembly: true
+            },
             content: [
-                'First paragraph',
-                'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+                {
+                    text: 'REPORT',
+                    style: 'header'
+                },
+                {
+                    columns: [
+                        [
+                            {
+                                columns: [
+                                    {
+                                        ul: [
+                                            'item 1',
+                                            'item 2',
+                                            'item 3'
+                                        ]
+                                    },
+                                    { text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Malit profecta versatur nomine ocurreret multavit, officiis viveremus aeternum superstitio suspicor alia nostram, quando nostros congressus susceperant concederetur leguntur iam, vigiliae democritea tantopere causae, atilii plerumque ipsas potitur pertineant multis rem quaeri pro, legendum didicisse credere ex maluisset per videtis. Cur discordans praetereat aliae ruinae dirigentur orestem eodem, praetermittenda divinum. Collegisti, deteriora malint loquuntur officii cotidie finitas referri doleamus ambigua acute. Adhaesiones ratione beate arbitraretur detractis perdiscere, constituant hostis polyaeno. Diu concederetur.' },
+                                    {
+                                        style: 'tableExample',
+                                        table: {
+                                            body: [
+                                                ['承認', '確認', '作成'],
+                                                [' ', ' ', ' ']
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        ]
+                    ]
+                },
             ],
-        };
+            styles: {
+                header: {
+                    fontSize: 18,
+                    bold: true
+                },
+            }
+        });
+
+
+
+        const printer: PdfPrinter = new PdfPrinter(fonts);
+        const docDefinition: any = JSON.parse(report) as TDocumentDefinitions;
 
         return new Promise((resolve, reject) => {
             const pdfDoc = printer.createPdfKitDocument(docDefinition);
@@ -31,7 +80,7 @@ export class PdfService {
             pdfDoc.on('data', (chunk) => chunks.push(chunk));
             pdfDoc.on('end', () => resolve(Buffer.concat(chunks)));
             pdfDoc.on('error', reject);
-            
+
             pdfDoc.end();
         });
     }
